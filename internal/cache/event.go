830dd31945aa7f22643362186bacc649ea483b6c
errodion <r.kuzmin.w@gmail.com>
2025-11-28 00:04:17 +0300

    Methods for calculate job

--- internal/cache/event.go
+++ internal/cache/event.go
@@ -18,7 +18,8 @@ const (
 
 type EventCacheClient interface {
 	CleanupOldEventsFromCache(context.Context) error
-	SaveNewEvents(context.Context, *[]dto.Event) error
+	SaveNewEvents(context.Context, *[]dto.Event) (*[]string, error)
+	GetUserEventsTimestampsByTypeAndRange(context.Context, string, dto.EventType, time.Time, time.Time) (*[]time.Time, error)
 }
 
 type eventCacheClient struct {
@@ -37,29 +38,75 @@ func NewEventCacheClient(storage *redis.Client) (EventCacheClient, error) {
 }
 
 func (client *eventCacheClient) CleanupOldEventsFromCache(ctx context.Context) error {
+	var keys []string
+	iter := client.storage.Scan(ctx, 0, fmt.Sprintf("%s:*:*", eventKeyPrefix), 0).Iterator()
 
-	_, err := client.storage.ZRemRangeByScore(
-		ctx, fmt.Sprintf("%s:*:*", eventKeyPrefix),
-		"0", strconv.FormatInt(time.Now().Add(-dto.MaxTimeRange).Unix(), 10),
-	).Result()
+	for iter.Next(ctx) {
+		keys = append(keys, iter.Val())
+	}
 
-	return err
+	if err := iter.Err(); err != nil {
+		return err
+	}
+
+	for _, key := range keys {
+		_, err := client.storage.ZRemRangeByScore(
+			ctx, key,
+			"0", strconv.FormatInt(time.Now().Add(-dto.MaxTimeRange).Unix(), 10),
+		).Result()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
-func (client *eventCacheClient) SaveNewEvents(ctx context.Context, events *[]dto.Event) error {
+func (client *eventCacheClient) SaveNewEvents(ctx context.Context, events *[]dto.Event) (*[]string, error) {
+	usersSet := make(map[string]bool)
+	users := make([]string, 0)
 	pipeline := client.storage.Pipeline()
-
 	for _, event := range *events {
 		pipeline.ZAdd(
 			ctx, fmt.Sprintf("%s:%s:%s", eventKeyPrefix, event.Actor, event.EventType),
 			redis.Z{
 				Score:  float64(event.Timestamp.Unix()),
-				Member: event.ID,
+				Member: float64(event.Timestamp.Unix()),
 			},
 		)
+		if _, exists := usersSet[event.Actor]; !exists {
+			users = append(users, event.Actor)
+			usersSet[event.Actor] = true
+		}
 	}
 
 	_, err := pipeline.Exec(ctx)
 
-	return err
+	return &users, err
+}
+
+func (client *eventCacheClient) GetUserEventsTimestampsByTypeAndRange(ctx context.Context, user string, eventType dto.EventType, startRange, endRange time.Time) (*[]time.Time, error) {
+	var timestamps []time.Time
+
+	timestampStrs, err := client.storage.ZRangeByScore(
+		ctx,
+		fmt.Sprintf("%s:%s:%s", eventKeyPrefix, user, eventType),
+		&redis.ZRangeBy{
+			Min: strconv.FormatInt(startRange.Unix(), 10),
+			Max: strconv.FormatInt(endRange.Unix(), 10),
+		},
+	).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, timestampStr := range timestampStrs {
+		unixTime, err := strconv.ParseInt(timestampStr, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse timestamp: %w", err)
+		}
+		timestamps = append(timestamps, time.Unix(unixTime, 0))
+	}
+
+	return &timestamps, nil
 }
